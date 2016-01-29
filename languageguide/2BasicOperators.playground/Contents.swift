2a6574af2c58c0ec0f850a2911f45b4c30bbf96e
Ali El Baitam <Ali@macmini.private>
2016-01-29 13:05:35 -0500

    2 - Basic Operators chapter in Language Guide

---
+++ languageguide/2BasicOperators.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//: # Basic Operators
+
+/*:
+“An operator is a special symbol or phrase that you use to check, change, or combine values.”
+*/

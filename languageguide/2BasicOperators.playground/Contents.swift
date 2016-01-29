fc94c1ac44fc8818f98a83deaa10c61dfebfe743
Ali Baitam <abaitam@gmail.com>
2016-01-29 13:46:50 -0500

    Floating point remainder section

--- languageguide/2BasicOperators.playground/Contents.swift
+++ languageguide/2BasicOperators.playground/Contents.swift
@@ -21,4 +21,37 @@ import UIKit
 * Unary - operate on a single target prefix (!b) or postfix (i++)
 * Binary - operate on two targets. infix
 * ternary - operate on three targets. One ternary operator: a ? b : c
+
+The values that operators affect are operands.
 */
+
+//: # Assignment Operator
+/*:
+The assignment operator (a = b) initializes or updates the values of a with the value of b
+*/
+
+let b = 10
+var a = 5
+a = b
+
+//: ## Decomposition
+let (x, y) = (1, 2)
+
+//: The assignment operator in Swift does not return a value
+
+//: ## Arithmetic Operators
+/*:
++, -, *, /
++ is supported for string concatenation
+*/
+"hello" + "world"
+
+//: ## Remainder Operator
+//: a = (b X some multiplier) + remainder
+9 % 4
+-9 % 4
+
+//: a % b == a % -b
+
+//: ## Floating-Point Remainder Calculations
+

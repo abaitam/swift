7185e350fd0f8afff8ce984a7f9e5d04a62d9bb9
Ali El Baitam <Ali@macmini.private>
2016-01-29 13:29:17 -0500

    terminology section

--- languageguide/2BasicOperators.playground/Contents.swift
+++ languageguide/2BasicOperators.playground/Contents.swift
@@ -5,5 +5,20 @@ import UIKit
 //: # Basic Operators
 
 /*:
-“An operator is a special symbol or phrase that you use to check, change, or combine values.”
+
+* An operator is a special symbol or phrase that you use to check, change, or combine values.
+* +, -, *, /, %, &&, detect and disallow value overflow (use Overflow operators)
+* % can be performed on floating-point numbers.
+* Two range operators: a..<b a...b
+* The assignment operator (=) does not return a value, to prevent it from being mistakenly used when the equal to operator (==) is intended.
+
+*/
+
+
+//: # Terminology
+/*:
+- Operators are unary, binary and ternary:
+* Unary - operate on a single target prefix (!b) or postfix (i++)
+* Binary - operate on two targets. infix
+* ternary - operate on three targets. One ternary operator: a ? b : c
 */
